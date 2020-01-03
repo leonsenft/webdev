3ec2840132eb3d8d7f81f1c413e452ea57890007
Jacob MacDonald <jakemac@google.com>
2020-01-03 12:29:20 -0800

    add bootstrapping

--- webdev/lib/src/frontend_server/frontend_asset_server.dart
+++ webdev/lib/src/frontend_server/frontend_asset_server.dart
@@ -1,10 +1,16 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
 
+import 'package:path/path.dart' as p;
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
+import 'package:shelf_static/shelf_static.dart';
 
 import '../daemon_client.dart';
+import 'ddc_names.dart';
+
+final _sdkDir = p.dirname(p.dirname(Platform.resolvedExecutable));
 
 class FrontendAssetServer {
   final HttpServer _server;
@@ -17,7 +23,13 @@ class FrontendAssetServer {
     await _serverPortFile.create(recursive: true);
     await _serverPortFile.writeAsString(_assetServerPort.toString());
 
-    var server = await serve(_handleRequest, 'localhost', _assetServerPort);
+    var cascade = Cascade()
+        .add(_sdkSourcesHandler())
+        .add(createStaticHandler(p.current, defaultDocument: 'index.html'))
+        .add(_handleJsModuleRequest)
+        .add(_handleJsAppRequest);
+
+    var server = await serve(cascade.handler, 'localhost', _assetServerPort);
 
     return FrontendAssetServer._(server);
   }
@@ -27,7 +39,66 @@ class FrontendAssetServer {
   }
 }
 
-FutureOr<Response> _handleRequest(Request request) {
-  print(request);
-  return Response.notFound('Unimplemented');
+Future<Response> _handleJsModuleRequest(Request request) async {
+  if (!request.requestedUri.path.endsWith('.dart.js')) {
+    return Response.notFound('Not Found');
+  }
+  var absolutePath = p.absolute(request.requestedUri.path.substring(1));
+  var jsonFile = File('${p.withoutExtension(absolutePath)}.dill.json');
+  var info = jsonDecode(await jsonFile.readAsString())[absolutePath];
+  if (info == null) {
+    return Response.notFound('Not Found');
+  }
+
+  var sourcesFile = File('${p.withoutExtension(absolutePath)}.dill.sources');
+  var sourcesBytes = await sourcesFile.readAsBytes();
+  var sourceBytes = sourcesBytes
+      .getRange(info['code'][0] as int, info['code'][1] as int)
+      .toList();
+  return Response.ok(utf8.decode(sourceBytes), headers: {
+    HttpHeaders.contentTypeHeader: 'application/javascript',
+  });
+}
+
+Future<Response> _handleJsAppRequest(Request request) async {
+  if (!request.requestedUri.path.endsWith('.js')) {
+    return Response.notFound('Not Found');
+  }
+  return Response.ok(_appBootstrap(request), headers: {
+    HttpHeaders.contentTypeHeader: 'application/javascript',
+  });
+}
+
+Handler _sdkSourcesHandler() {
+  var sdkServer = createStaticHandler(_sdkDir);
+  return (request) {
+    if (request.requestedUri.pathSegments[1] != r'$sdk') {
+      return Response.notFound('Not Found');
+    }
+    return sdkServer(Request(
+        request.method,
+        request.requestedUri
+            .replace(pathSegments: request.requestedUri.pathSegments.skip(2))));
+  };
+}
+
+String _appBootstrap(Request request) {
+  var relativePath = request.requestedUri.path.substring(1);
+  var basename = p.url.basename(request.requestedUri.path);
+  var appModuleName = '${p.withoutExtension(basename)}.dart';
+  var moduleScope =
+      pathToJSIdentifier(p.absolute(p.url.withoutExtension(relativePath)));
+  return '''
+require.config({
+    waitSeconds: 0,
+    paths: {"dart_sdk": "\$sdk/lib/dev_compiler/kernel/amd/dart_sdk"}
+});
+
+require(["$appModuleName", "dart_sdk"], function(app, dart_sdk) {
+  dart_sdk.dart.setStartAsyncSynchronously(true);
+  dart_sdk._isolate_helper.startRootIsolate(() => {}, []);
+  /* MAIN_EXTENSION_MARKER */
+  app.$moduleScope.main();
+})();
+''';
 }
